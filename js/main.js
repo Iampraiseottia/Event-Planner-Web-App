7ee1bf6241e4016c52a04ea9750646844e85cf58
OTTIA PRAISE BETECK <ottiapraise50@gmail.com>
2024-05-17 16:52:41 -0700

    main and about completed

---
+++ js/main.js
@@ -0,0 +1,42 @@
+//About Section Appear
+document.addEventListener("DOMContentLoaded", function () {
+    var aboutSection = document.querySelector("#About");
+  
+    var options = {
+      threshold: 0.45,
+    };
+  
+    var observer = new IntersectionObserver(function (entries, observer) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+  
+    observer.observe(aboutSection);
+});
+  
+
+//Call Section Appear
+document.addEventListener("DOMContentLoaded", function () {
+    var aboutSection = document.querySelector("#call-to-action");
+  
+    var options = {
+      threshold: 0.35,
+    };
+  
+    var observer = new IntersectionObserver(function (entries, observer) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+  
+    observer.observe(aboutSection);
+  });
+  
+
